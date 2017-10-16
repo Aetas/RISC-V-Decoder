b1e5ba0b0adeb153927ec13561c425d345eb0a9e
Derek Prince <ace.prince08@gmail.com>
2017-10-15 18:56:41 -0600

    Basically an initial commit mk. II.

--- RISC-V/src/main.rs
+++ RISC-V/src/main.rs
@@ -1,3 +1,14 @@
+extern crate getopts;
+use getopts::Options;   //argument options
+use std::env;           //environment variables
+
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+
+
     println!("Hello, world!");
 }
